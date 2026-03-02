10fbb9c344f9627ce63f0011212552dbaaf42fcf
sunnykardak <skardak9@gmail.com>
2026-03-02 16:21:03 +0530

    Create Anish.py

---
+++ Anish.py
@@ -0,0 +1,12 @@
+
+I am getting AccessDenied while assuming role
+svc-role-real-time-transformation-poc.
+My current role is
+App-Developer-fGLBAWSDDPConsDevUAT.
+
+It seems either:
+	•	sts:AssumeRole permission is missing on my role
+OR
+	•	Trust relationship is not configured on target role
+
+Could you please check and allow my role to assume this role?
